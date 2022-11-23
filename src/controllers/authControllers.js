b3fa86f1307ce8d50be02174f9777022e74997ec
¨higorpr¨ <higorpr@gmail.com>
2022-11-23 04:08:43 -0300

    feat: Connect app to mongoDB server & initial creation of login route

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,5 @@
+import { usersCollection, sessionsCollection } from "../database/db.js";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+export async function login(req, res) {}
---
+++ src/middlewares/global/retrieveUserFromToken.js
@@ -0,0 +1,31 @@
+import { usersCollection, sessionsCollection } from "../../database/db.js";
+
+export async function retrieveUserFromToken(req, res, next) {
+    ```
+    This function receives the token as req.headers and returns the 
+    user linked with the token's session if the session exists.
+    ```;
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "").trim();
+    let user;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const userSession = await sessionsCollection.findOne({ token: token });
+
+        if (!userSession) {
+            return res.sendStatus(404);
+        }
+
+        user = await usersCollection.findOne({ _id: userSession.userId });
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(502);
+    }
+
+    res.locals.user = user;
+}
